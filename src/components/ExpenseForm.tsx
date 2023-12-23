eb34e20e037393529fa82d74ec763865b7bfe380
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-23 15:34:00 -0500

    building the expense form

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="desciprtion" className="form-label">
+          Desciprtion
+        </label>
+        <input id="desciprtion" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input id="amount" type="number" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select id="catergory" className="form-select"></select>
+        <option value=""></option>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
