73ba966f45970051fcedd4e6657b5a4f66fe681c
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-23 10:41:18 -0500

    first commit

---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: Props) => {
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expenses) => (
+          <tr key={expenses.id}>
+            <td>{expenses.description}</td>
+            <td>{expenses.amount}</td>
+            <td>{expenses.category}</td>
+            <td>
+              <button
+                className="btn btn outline-danger"
+                onClick={() => onDelete(expenses.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ExpenseList;
