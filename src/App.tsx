e8336f8d16465b8c62dba2f0f9a4dc9ebf85f832
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-23 11:06:39 -0500

    building the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
     { id: 3, description: "ccc", amount: 10, category: "Utilities" },
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-  ];
+  ]);
   return (
     <div>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log("delete", id)}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
   );
--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -12,6 +12,7 @@ interface Props {
 }
 
 const ExpenseList = ({ expenses, onDelete }: Props) => {
+  if (expenses.length === 0) return null;
   return (
     <table className="table table-bordered">
       <thead>
