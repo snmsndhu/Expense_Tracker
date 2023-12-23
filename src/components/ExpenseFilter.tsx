c48f7f8a72af4db53c7019908ac51e6ec206d144
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-23 11:53:32 -0500

    building the filter component

---
+++ src/components/ExpenseFilter.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ExpenseFilter = () => {
+  return (
+    <select className="form-select">
+        <option value="">All category</option>
+        <option value="Groceries">Groceries</option>
+        <option value="Utilities">Utilities</option>
+        <option value="Entertainment">Entertainment</option>
+    </select>>
+  )
+}
+
+export default ExpenseFilter
